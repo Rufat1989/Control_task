7af475889a62acab043df3418c11def4b57fdb0b
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-26 13:04:57 +0600

    added task

---
+++ task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+string[] array = new string[5] {"Russia", "Denmark", "Kazan", "123", "-2"};
+string[] arrayres = new string[array.Length];
+void GetArray(string[] array, string[] arrayres)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        arrayres[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+GetArray(array, arrayres);
+PrintArray(arrayres);
